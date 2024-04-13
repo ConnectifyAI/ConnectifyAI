9b9d4c229045c5ab2dd0b4783961996b0dfca939
suri312006 <suri312006@gmail.com>
2024-04-13 09:39:00 -0700

    working on google hadnler

--- src/routes/auth/callback/google/+server.ts
+++ src/routes/auth/callback/google/+server.ts
@@ -1,4 +1,21 @@
+import { github } from "$lib/server/auth";
+import type { RequestEvent } from "./$types";
 
-import { github, lucia } from "$lib/server/auth";
-import { OAuth2RequestError } from "oslo/oauth2";
-import Something from "$components/somehting.svelte"
+
+export async function GET(event: RequestEvent): Promise<Response> {
+    const code = event.url.searchParams.get('code');
+    const state = event.url.searchParams.get('state');
+
+    const storedState = event.cookies.get('github_oauth_state') ?? null;
+
+    if (!code || !state || !storedState || state !== storedState) {
+        return new Response(null, {
+            status: 400
+        })
+    }
+
+    try{
+        const tokens = await github.validateAuthorizationCode(code)
+    }
+
+}
