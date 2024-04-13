b81702905f6c9a4a40d29930901c0557e04185f6
suri312006 <suri312006@gmail.com>
2024-04-12 23:40:29 -0700

    finish auth tomorrow

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,5 +3,8 @@ import { dev } from '$app/environment'
 import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
 import { db } from './db';
 import { session, user } from './db/schema';
+import { Github, Google } from 'artic';
 
 const adapter = new DrizzlePostgreSQLAdapter(db, session, user);
+//https://github.com/bmdavis419/SvelteKit-Ecommerce/blob/main/src/lib/server/auth.ts
+export const github = new Github
