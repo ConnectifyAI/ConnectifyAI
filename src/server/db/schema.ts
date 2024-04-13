e1cd5657e1c070898a333f3b29ef6d3a64f13adc
suri312006 <suri312006@gmail.com>
2024-04-12 21:39:03 -0700

    db setup mostly done

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,5 +1,65 @@
-import { pgTable, serial } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgTable, serial, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
-    id: serial('id').primaryKey(),
+    id: uuid('id').defaultRandom().primaryKey().notNull(),
+
 })
+
+export const nodes = pgTable('nodes', {
+    id: uuid('id').defaultRandom().primaryKey().notNull(),
+
+})
+
+export const edges = pgTable('edges', {
+    id: uuid('id').defaultRandom().primaryKey().notNull(),
+})
+
+export const nodesRelations = relations(nodes, ({many}) => ({
+    edges: many(edges)
+}))
+
+export const edgesRelations = relations(edges, ({one}) =>({
+    tail: one(nodes),
+    head: one(nodes),
+
+}))
+
+
+// export const userRelations = relations(users, ({ many }) => ({
+//   ownedTests: many(tests),
+//   ownedSteps: many(steps)
+//
+//   //TODO: maybe we could do updated tests
+// }))
+//
+//
+// export const testRelations = relations(tests, ({ many, one }) => ({
+//
+//   author: one(users, {
+//     fields: [tests.author_id],
+//     references: [users.id]
+//   }),
+//
+//   updatedBy: one(users, {
+//     fields: [tests.updatedBy],
+//     references: [users.id]
+//   }),
+//
+//   steps: many(steps)
+//
+// }))
+//
+// export const stepRelations = relations(steps, ({ one }) => ({
+//
+//   author: one(users, {
+//     fields: [steps.author_id],
+//     references: [users.id]
+//   }),
+//
+//   parent_test: one(tests, {
+//     fields: [steps.test_id],
+//     references: [tests.id]
+//   }),
+//
+// }))
