2f061f3dffe9cf0d89d18af5f651a4b2802ab583
suri312006 <suri312006@gmail.com>
2024-04-13 14:07:51 -0700

    fix? for github?

--- src/routes/auth/callback/github/+server.ts
+++ src/routes/auth/callback/github/+server.ts
@@ -18,77 +18,77 @@ export async function GET(event: RequestEvent): Promise<Response> {
         });
     }
 
-    // try {
-    const tokens = await github.validateAuthorizationCode(code);
-    const githubUserResponse = await fetch('https://api.github.com/user', {
-        headers: {
-            Authorization: `Bearer ${tokens.accessToken}`
-        }
-    });
-
-    const githubUser: GitHubUser = await githubUserResponse.json();
-
-    const existingUser = await db.query.user.findFirst({
-        where: and(eq(user.provider, 'github'), eq(user.providerId, githubUser.id))
-    })
-
-    if (existingUser) {
-        const session = await lucia.createSession(existingUser.id, {})
-        const sessionCookie = lucia.createSessionCookie(session.id)
-        event.cookies.set(sessionCookie.name, sessionCookie.value, {
-            path: '.',
-            ...sessionCookie.attributes
-        });
-    } else {
-        const githubEmailResponse = await fetch('https://api.github.com/user/emails', {
+    try {
+        const tokens = await github.validateAuthorizationCode(code);
+        const githubUserResponse = await fetch('https://api.github.com/user', {
             headers: {
                 Authorization: `Bearer ${tokens.accessToken}`
             }
         });
 
-        const githubEmail: GitHubEmail[] = await githubEmailResponse.json();
+        const githubUser: GitHubUser = await githubUserResponse.json();
 
-        const primary = githubEmail.find((entry) => entry.primary);
+        const existingUser = await db.query.user.findFirst({
+            where: and(eq(user.provider, 'github'), eq(user.providerId, githubUser.id))
+        })
 
-        if (primary) {
-            const nameParts = githubUser.name.split(' ');
-            const userId = generateId(40);
-
-            await db.insert(user).values({
-
-                id: userId,
-                provider: 'github',
-                providerId: githubUser.id,
-                email: primary.email,
-                firstName: nameParts[0] ?? '',
-                lastName: nameParts[1] ?? '',
-                isAdmin: false
-            })
-            const session = await lucia.createSession(userId, {})
+        if (existingUser) {
+            const session = await lucia.createSession(existingUser.id, {})
             const sessionCookie = lucia.createSessionCookie(session.id)
             event.cookies.set(sessionCookie.name, sessionCookie.value, {
                 path: '.',
                 ...sessionCookie.attributes
             });
-        }
+        } else {
+            const githubEmailResponse = await fetch('https://api.github.com/user/emails', {
+                headers: {
+                    Authorization: `Bearer ${tokens.accessToken}`
+                }
+            });
 
-    }
-    return new Response(null, {
-        status: 302,
-        headers: {
-            Location: '/'
+            const githubEmail: GitHubEmail[] = await githubEmailResponse.json();
+
+            const primary = githubEmail.find((entry) => entry.primary);
+
+            if (primary) {
+                const nameParts = githubUser.name.split(' ') ?? ["unknown", "unknown"];
+                const userId = generateId(40);
+
+                await db.insert(user).values({
+
+                    id: userId,
+                    provider: 'github',
+                    providerId: githubUser.id,
+                    email: primary.email,
+                    firstName: nameParts[0] ?? '',
+                    lastName: nameParts[1] ?? '',
+                    isAdmin: false
+                })
+                const session = await lucia.createSession(userId, {})
+                const sessionCookie = lucia.createSessionCookie(session.id)
+                event.cookies.set(sessionCookie.name, sessionCookie.value, {
+                    path: '.',
+                    ...sessionCookie.attributes
+                });
+            }
+
         }
-    });
-    // } catch (e) {
-    //     if (e instanceof OAuth2RequestError) {
-    //         return new Response(null, {
-    //             status: 400
-    //         })
-    //     }
-    //     return new Response(null, {
-    //         status: 500
-    //     })
-    // }
+        return new Response(null, {
+            status: 302,
+            headers: {
+                Location: '/'
+            }
+        });
+    } catch (e) {
+        if (e instanceof OAuth2RequestError) {
+            return new Response(null, {
+                status: 400
+            })
+        }
+        return new Response(null, {
+            status: 500
+        })
+    }
 }
 
 
