2f4d2a108e248a1994ace47c4f91cd0f11a6c9fa
suri312006 <suri312006@gmail.com>
2024-04-14 13:44:45 -0700

    pushsehd

--- src/routes/canvas-test/+page.server.ts
+++ src/routes/canvas-test/+page.server.ts
@@ -1,37 +1,34 @@
-import type { PageServerLoad } from './$types';
-import { downloadFile } from '@huggingface/hub';
 import type { Actions } from './$types';
-import { searchModels } from '$lib/server/hf/model';
-import { getDatasetInfo, searchDatasets } from '$lib/server/hf/dataset';
+import { getDatasetInfo, } from '$lib/server/hf/dataset';
 
 
-export const load: PageServerLoad = async ({ params }) => {
-    //TODO: load their latest
-
-}
+// export const load: PageServerLoad = async ({ params }) => {
+//     //TODO: load their latest
+//
+// }
 
 export const actions = {
-	searchForDataset: async ({ locals, request }) => {
-		//search lmfao
-		const formData = await request.formData()
-		const query = formData.get('query')
+    searchForDataset: async ({ request }) => {
+        //search lmfao
+        const formData = await request.formData()
+        const query = formData.get('query')
 
-		if (!query) {
-			throw new Error("query not provided")
-		}
+        if (!query) {
+            throw new Error("query not provided")
+        }
 
-		let datasets = await searchDatasets(query.toString())
+        // let datasets = await searchDatasets(query.toString())
 
         let specialOne = await getDatasetInfo("soemthing")
 
 
         //TODO: do the creation of nodes that you need?
 
-		return {
+        return {
             datasets: [specialOne]
-		}
+        }
 
-	},
+    },
 } satisfies Actions;
 
 
