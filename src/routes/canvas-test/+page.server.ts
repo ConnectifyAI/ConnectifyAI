bf167fe270adff7c25c683af24e185efcdba0474
suri312006 <suri312006@gmail.com>
2024-04-14 18:58:11 -0700

    inputs and outputs models done!

--- src/routes/canvas-test/+page.server.ts
+++ src/routes/canvas-test/+page.server.ts
@@ -1,11 +1,15 @@
 import type { PageServerLoad, Actions } from './$types';
-import { getDatasetInfo } from '$lib/server/hf/dataset';
+import { getDatasetInfo, searchDatasets } from '$lib/server/hf/dataset';
+import { getModelInfo } from '$lib/server/hf/model';
 
 export const load: PageServerLoad = async ({ params }) => {
 	//TODO: load their latest
-	const datasetInfo = await getDatasetInfo('change later');
 
-	return { datasetInfo };
+	const datasetInfo = await getDatasetInfo('change later');
+	const modelInfo = await getModelInfo('change later');
+
+	return { datasetInfo, modelInfo };
+
 };
 
 export const actions = {
@@ -27,5 +31,26 @@ export const actions = {
 		return {
 			datasets: [specialOne]
 		};
+	},
+
+	searchForModel: async ({ request }) => {
+		//search lmfao
+		const formData = await request.formData();
+		const query = formData.get('query');
+
+		if (!query) {
+			throw new Error('query not provided');
+		}
+
+		let datasets = await searchDatasets(query.toString())
+
+		// const specialOne = await getDatasetInfo('soemthing');
+		//
+		// //TODO: do the creation of nodes that you need?
+
+		return {
+			datasets
+		};
 	}
+
 } satisfies Actions;
