3494dc48dd1861c8a4a33fcc28a3d8f0baa3b3fc
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-04-14 17:15:13 -0700

    removed das

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,14 +4,13 @@ import { env } from "process";
 
 if (!env.DATABASE_URL) {
 	throw new Error("invalid env")
-	
 }
 
 export default {
-  schema: "./src/lib/server/db/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  },
-  tablesFilter: ["qagent_*"],
+	schema: "./src/lib/server/db/schema.ts",
+	driver: "pg",
+	dbCredentials: {
+		connectionString: env.DATABASE_URL,
+	},
+	tablesFilter: ["qagent_*"],
 } satisfies Config;
