3fac9d8a548afed2b8a8eea8ebef7d7ca5c02552
Alb111 <albertfelix2005@gmail.com>
2024-04-18 12:06:38 -0700

    UI stuff

--- src/lib/components/Modal/SubCompments/ResultCard.svelte
+++ src/lib/components/Modal/SubCompments/ResultCard.svelte
@@ -8,17 +8,17 @@
 </script>
 
 <div class="wrapper">
-	<section class="flex justify-between items-center">
+	<section class="flex max-w-[55 rem] justify-between items-center">
 		<aside class="">
-			<h1>{repoId}</h1>
+			<h1 class="truncate text-sm max-w-80">{repoId}</h1>
 			<p class="opacity-30">by {author}</p>
 		</aside>
 
 		<button class="btn-md variant-outline rounded-md border-2 border-blue-500">Add Dataset</button>
 	</section>
-	<section class="= grid grid-row-1 grid-cols-2 gap-2 text-sm p-2">
+	<section class= "flex gap-2 text-sm p-2 overflow-x-auto overflow-hidden">
 		{#each outputFeatures as feature}
-			<p class=" px-1 min-w-24 bg-slate-100 rounded-sm flex justify-center items-center text-[14px]">{feature.label+": "+feature.dtype}</p>
+			<p class=" px-1 w-23 bg-slate-100 rounded-sm flex justify-center items-center text-[14px]">{feature.label+": "+feature.dtype}</p>
 		{/each}
 
 	</section>
