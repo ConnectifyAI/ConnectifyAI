078fc4ad9a4453ffc3e3f3edc5df5916f26063b9
suri312006 <suri312006@gmail.com>
2024-04-17 19:24:16 -0700

    removed sidebar border

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import '../app.postcss'
-	import { Navbar} from '$components/Navigation'
+	import { Navbar } from '$components/Navigation'
 
 	import { initializeStores } from '@skeletonlabs/skeleton'
 	import { AppShell } from '@skeletonlabs/skeleton'
 
 	// MODAL
-	import { Modal, getModalStore } from '@skeletonlabs/skeleton'
-	import type { ModalSettings, ModalComponent, ModalStore } from '@skeletonlabs/skeleton'
+	import { Modal } from '@skeletonlabs/skeleton'
+	import type { ModalComponent } from '@skeletonlabs/skeleton'
 
 	import NewModel from '$components/Modal/NewModel.svelte'
 	import NewDataset from '$components/Modal/NewDataset.svelte'
