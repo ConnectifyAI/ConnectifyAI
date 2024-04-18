c16bd2970a28934a4e335df18bbb5bda028a9a70
Mansi Saini <mansisaini26@gmail.com>
2024-04-18 16:35:42 -0700

    navbar heading

--- src/lib/components/Navigation/Navbar.svelte
+++ src/lib/components/Navigation/Navbar.svelte
@@ -6,6 +6,18 @@
 
 </script>
 
+<style>
+	.heading {
+  color: black;
+  font-family: Poppins;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 29px;
+  letter-spacing: 0em;
+  text-align: left;
+}
+</style>
+
 <div class="">
 	<AppBar
 		gridColumns="grid-cols-3"
@@ -15,16 +27,16 @@
 		shadow="shadow-xl"
 	>
 		<svelte:fragment slot="lead">
-			<div class="flex text-3xl px-10 py-2 h-13">
+			<div class="flex items-center px-10 py-2 h-13">
 				<div class="bg-blue-700 px-2 py-2 rounded-xl"><SendToBack size="30" color="white" /></div>
-				<h1 class="ml-5 mt-1 font-poppins text-black">ConnectifyAI</h1>
+				<h3 class="heading ml-5">ConnectifyAI</h3>
 			</div>
 		</svelte:fragment>
 		<svelte:fragment slot="trail">
 			<div class="flex space-x-5">
 				<div class="flex justify-between">
 					<Download size="30" class="" color="black" />
-					<h1 class="text-2xl ml-3 font-dmMono text-black">Download</h1>
+					<h1 class="text-lg ml-3 font-dmMono text-black">Download</h1>
 				</div>
 				<div class="flex">
 					<div class="w-0.5 h-full bg-black mr-5"></div>
