69fd440c1f25b60d5009d2afaff39daa1c5d32f5
Alb111 <albertfelix2005@gmail.com>
2024-04-18 13:08:19 -0700

    uyoiuyoiu

--- src/lib/components/Modal/DatasetModal.svelte
+++ src/lib/components/Modal/DatasetModal.svelte
@@ -25,9 +25,7 @@
 	}
 
 	const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
-	// arr of obj, pass into result card
-
+		
 </script>
 
 <div class="wrapper">
--- src/lib/components/Modal/SubCompments/ResultCard.svelte
+++ src/lib/components/Modal/SubCompments/ResultCard.svelte
@@ -3,12 +3,11 @@
 	import type { DatasetInfo, ModelInfo } from '$lib/server/helpers/apiTypes'
 	export let info: DatasetInfo | ModelInfo
 	// Model | Dataset
-	const { author, repoId , outputFeatures, downloads, likes, createdAt } = info
-
+	const { author, repoId, outputFeatures, downloads, likes, createdAt } = info
 </script>
 
 <div class="wrapper">
-	<section class="flex max-w-[55 rem] justify-between items-center">
+	<section class="flex max-w-[55rem] justify-between items-center">
 		<aside class="">
 			<h1 class="truncate text-sm max-w-80">{repoId}</h1>
 			<p class="opacity-30">by {author}</p>
@@ -16,11 +15,12 @@
 
 		<button class="btn-md variant-outline rounded-md border-2 border-blue-500">Add Dataset</button>
 	</section>
-	<section class= "flex gap-2 text-sm p-2 overflow-x-auto overflow-hidden">
+	<section class="flex gap-2 text-sm p-2 overflow-x-auto overflow-hidden">
 		{#each outputFeatures as feature}
-			<p class=" px-1 w-23 bg-slate-100 rounded-sm flex justify-center items-center text-[14px]">{feature.label+": "+feature.dtype}</p>
+			<p class=" px-1 w-23 bg-slate-100 rounded-sm flex justify-center items-center text-[14px]">
+				{feature.label + ': ' + feature.dtype}
+			</p>
 		{/each}
-
 	</section>
 
 	<!-- stats -->
