f6550b46b52abc17bcfcac5c7ddc691ea7d2cbcd
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-04-18 14:45:20 -0700

    graphId added

--- src/routes/proc/canvas-test/+page.svelte
+++ src/routes/proc/canvas-test/+page.svelte
@@ -14,12 +14,13 @@
 		bgColor
 	} from '$routes/proc/canvas-test/Dataset.js'
 
-	import { nodes, edges } from '$stores/graph'
+	import { nodes, edges, graphId } from '$stores/graph'
 
 	export let data
 
 	nodes.set(data.graph.nodes)
 	edges.set(data.graph.edges)
+	graphId.set(data.graph.id)
 
 	// $: console.log('data', data.graph.nodes)
 	// $: console.log('edges', data.graph.edges)
