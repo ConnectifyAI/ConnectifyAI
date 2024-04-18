fe75f6cd2f51951254d65cd62ea8611cf7eae1bb
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2024-04-17 21:03:44 -0700

    modal popup

--- src/lib/components/Modal/DatasetModal.svelte
+++ src/lib/components/Modal/DatasetModal.svelte
@@ -1,29 +1,17 @@
 <script lang="ts">
-	import { Stepper, Step } from '@skeletonlabs/skeleton'
-	import StepOne from './SubCompments/StepOne.svelte'
-	import StepTwo from './SubCompments/StepTwo.svelte'
-	import StepThree from './SubCompments/StepThree.svelte'
-
 	export let parent: any
 	console.log('parent', parent)
+
+	const arr = [1, 2, 3, 4, 5]
 </script>
 
 <div
 	class="flex min-w-[50rem] min-h-[30rem] bg-slate-100 p-16 rounded-md border-slate-700 border-4"
 >
-	<!-- <Stepper>
-		<Step>
-			<svelte:fragment slot="header">(header)</svelte:fragment>
-			(content)
-		</Step>
-		<Step>
-			<svelte:fragment slot="header">(header)</svelte:fragment>
-			(content)
-		</Step>
-	</Stepper> -->
-	<Stepper active="bg-blue-300" border="border-t border-black" badge="bg-gris" class="h-96">
-		<StepOne />
-		<StepTwo />
-		<StepThree />
-	</Stepper>
+	hello
+	<div>
+		{#each arr as item}
+			<p>{item}</p>
+		{/each}
+	</div>
 </div>
